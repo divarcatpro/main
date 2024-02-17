df47c939e4063ea33b9a73408fbf64180f8c166d
divarcatpro <159721064+divarcatpro@users.noreply.github.com>
2024-02-17 13:30:05 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let $ = document
+let loading = $.querySelector(".loader")
+
+function loadHandler() {
+	setInterval(function (){
+	loading.classList.add("hidden")
+	},8000)
+}
+
+window.addEventListener("load",loadHandler)
+setTimeout(function(){ loading.remove(); }, 10000);
